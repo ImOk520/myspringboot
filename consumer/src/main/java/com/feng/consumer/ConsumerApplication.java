11e9d3e9e94e3353e6bb0116e209bc9040d0f740
fengge3 <fengge3@iflytek.com>
2020-10-27 11:23:15 +0800

    spring admin集成

--- consumer/src/main/java/com/feng/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/feng/consumer/ConsumerApplication.java
@@ -7,6 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 /**
  * Feng, Ge 2020-10-13 20:46
@@ -15,10 +17,16 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.feng.client")
 @EnableHystrix
-//@EnableAdminServer
+@EnableAdminServer
 @EnableHystrixDashboard
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    //加入如下配置
+    @Bean({"threadPoolTaskExecutor", "webMvcAsyncTaskExecutor"})
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        return new ThreadPoolTaskExecutor();
     }
 }
