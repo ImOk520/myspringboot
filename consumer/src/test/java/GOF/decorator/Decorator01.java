70856062ff008ef1309c5acc4aea6409d9afd8a9
fengge3 <fengge3@iflytek.com>
2021-04-12 16:56:22 +0800

    refactoring code

---
+++ consumer/src/test/java/GOF/decorator/Decorator01.java
@@ -0,0 +1,11 @@
+package GOF.decorator;
+
+
+/**
+ * 如果不使用装饰模式
+ *
+ * Feng, Ge
+ */
+public class Decorator01 {
+
+}
---
+++ consumer/src/test/java/GOF/strategy/Strategy01.java
@@ -0,0 +1,16 @@
+package GOF.strategy;
+
+
+/**
+ *
+ * Feng, Ge
+ */
+public class Strategy01 {
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
